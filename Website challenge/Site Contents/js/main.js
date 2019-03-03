87a395f7e4e1f62919573477b4264e62b2186f83
Isaac Morrow <isaacsenseimorrow@gmail.com>
2019-03-02 17:12:29 -0800

    Started to add author information.

---
+++ Website challenge/Site Contents/js/main.js
@@ -0,0 +1,8 @@
+function author_details(name) {
+    if (name == "basran") {
+        document.getElementById("author-details").innerHTML = "Basran is cool I guess"
+    }
+
+    console.log("Penis");
+
+}
