d895bc3ea6da4c98f6f6411964394c6d17f44f86
9921459 <mediamanmorrow@gmail.com>
2019-03-07 13:35:18 -0800

    Started to add forms

--- Website challenge/Site Contents/js/main.js
+++ Website challenge/Site Contents/js/main.js
@@ -1,7 +1,17 @@
 function author_details(name) {
     if (name == "basran") {
-        document.getElementById("author-details").innerHTML = "Basran is cool I guess"
+        document.getElementById("author-details").innerHTML = "Basran is cool I guess";
     }
+    console.log("hello there world")
 
+    if (name == "fallis") {
+        document.getElementById("author-details").innerHTML = "Fallis is cool I guess"
+    }
     
+}
+
+function showInfo() {
+
+    var userElement = document.getElementById("username");
+    console.log(userElement);
 }
